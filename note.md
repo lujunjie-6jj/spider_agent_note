f64d81f20442cc9fd33dcefc9ce89b0f5be00b0d
lujunjie-6jj <1141761377@qq.com>
2018-08-18 13:41:49 +0800

    Just some exercises and notes

--- note.md
+++ note.md
@@ -1,3 +1,11 @@
 python 3.6
 
+urlopen返回对象：
+
+url='https://www.baidu.com/'
+    with request.urlopen(url) as rsq:
+    r = rsq.read()    
+    html=r.decode()
+
+chardet可以自动检测页面文件编码格式，但可能有误
 
--- urllib_0.py
+++ urllib_0.py
@@ -1,10 +1,19 @@
-from urllib import request
+from urllib import request,parse
 
 if __name__ == '__main__':
     url='https://www.baidu.com/'
-    with request.urlopen(url) as rsq:
+    wd=input('输入你的关键字：')
+    qs={'wd':wd}
+    #转化url编码
+    qs=parse.urlencode(qs)
+    fullurl=url+qs
+    print(qs)
+    print(fullurl)
+    with request.urlopen(fullurl) as rsq:
+
         print('url:',rsq.geturl())
         print('info:',rsq.info())
+        print('code:',rsq.getcode())    #状态码    200
         print('rsq类型:',type(rsq))
 
         r = rsq.read()
@@ -13,3 +22,4 @@ if __name__ == '__main__':
         html=r.decode()
         print(type(html))
         print('html:',html)
+
