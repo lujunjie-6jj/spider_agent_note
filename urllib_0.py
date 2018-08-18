a8c3edee5399baa37a93e4d57685623244f7184d
lujunjie-6jj <1141761377@qq.com>
2018-08-18 13:08:15 +0800

    Just some exercises and notes

--- urllib_0.py
+++ urllib_0.py
@@ -3,10 +3,13 @@ from urllib import request
 if __name__ == '__main__':
     url='https://www.baidu.com/'
     with request.urlopen(url) as rsq:
+        print('url:',rsq.geturl())
+        print('info:',rsq.info())
+        print('rsq类型:',type(rsq))
+
         r = rsq.read()
         print(type(r))
 
-
         html=r.decode()
         print(type(html))
-        print(html)
+        print('html:',html)
